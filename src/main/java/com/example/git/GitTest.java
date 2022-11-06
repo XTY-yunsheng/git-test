b784332ff29f10c84a2ab048d5ee0efa5ee65f6c
yunsheng <2765252307@qq.com>
2022-11-06 13:10:41 +0800

    first commit

---
+++ src/main/java/com/example/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @author yunsheng
+ * @create 2022-11-06 12:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
